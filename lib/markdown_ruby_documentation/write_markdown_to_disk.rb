b72dfe9740fd23f9a186ea29a2118c7e4244bfbd
Dustin Zeisler <dustin@zeisler.net>
2017-12-12 14:46:26 -0800

    Only Save file if content is different than current file

--- lib/markdown_ruby_documentation/write_markdown_to_disk.rb
+++ lib/markdown_ruby_documentation/write_markdown_to_disk.rb
@@ -6,24 +6,33 @@ module MarkdownRubyDocumentation
     def initialize(dir:, skip_if_blank: false, relative_dir:)
       @dir           = dir
       @skip_if_blank = skip_if_blank
-      @relative_dir = relative_dir
+      @relative_dir  = relative_dir
     end
 
     def call(name:, text:)
       return if skip_save?(text, name)
       name = name.gsub(dir, "").underscore
-      path = File.join(dir, name)
-      FileUtils.mkdir_p(path.split("/").tap { |p| p.pop }.join("/"))
-      File.open("#{path}.md", "w").write(text)
+      file = "#{name}.md"
+      path = File.join(dir, file)
+
+      return if file_exists_with?(text, path)
+      write_file(path, text)
     end
 
     private
 
-    def skip_save?(text, name)
+    def file_exists_with?(text, path)
+      File.exist?(path) && Digest::MD5.new.update(File.open(path).read) == Digest::MD5.new.update(text)
+    end
+
+    def write_file(path, text)
+      FileUtils.mkdir_p(path.split("/").tap { |p| p.pop }.join("/"))
+      File.open(path, "w").write(text)
+    end
+
+    def skip_save?(text, _name)
       if skip_if_blank
-        if Array(text.split("\n")[2..-1]).all? { |line| line.blank? }
-          return true
-        end
+        return true if Array(text.split("\n")[2..-1]).all?(&:blank?)
       end
     end
   end
